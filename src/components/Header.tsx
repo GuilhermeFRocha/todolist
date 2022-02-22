5b028daf9b4147cabc674a3cdb7627e89576c1b5
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-02-22 18:56:02 -0300

    estilizando

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="/logo.svg" alt="to.do"/>
+        <h1 style={{color:'white'}}>to-do list</h1>
       </div>
     </header>
   )
